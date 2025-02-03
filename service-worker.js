6dc5eb40adcf4d9cd02a8aa5f991ad74379b9c68
Meet Vyas <meetvyas1110@gmail.com>
2025-02-03 15:36:18 -0500

    fix: correct file paths in service worker and add GitHub Actions workflow for deployment

--- service-worker.js
+++ service-worker.js
@@ -2,11 +2,11 @@ const CACHE_NAME = 'to-do-pwa-cache-v1';
 const FILES_TO_CACHE = [
  '/WebDevTrends/',
  '/ WebDevTrends/index.html',
- '/WebDevTrends /style.css',
- '/WebDevTrends /app.js',
- '/WebDevTrends /manifest.json',
- '/WebDevTrends /icons/icon-128.png',
- '/WebDevTrends /icons/icon-512.png'
+ '/WebDevTrends/style.css',
+ '/WebDevTrends/app.js',
+ '/WebDevTrends/manifest.json',
+ '/WebDevTrend/icons/icon-128.png',
+ '/WebDevTrends/icons/icon-512.png'
 ];
 self.addEventListener('install', (event) => {
  event.waitUntil(
